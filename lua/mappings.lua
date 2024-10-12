9250be3e0dbcc748b78c6308998d76ba01768134
Phillip Smith <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-10-12 17:25:50 +1100

    NvChad v3.0 update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -47,6 +47,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- Menu ui for neovim: Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open("default")
+end, {})
+
+-- Menu ui for neovim: mouse users + nvimtree users
+vim.keymap.set("n", "<RightMouse>", function()
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
+  require("menu").open(options, { mouse = true })
+end, {})
+
 require "mappings.dap-python"
 require "mappings.dap-ui"
 require "mappings.git"
